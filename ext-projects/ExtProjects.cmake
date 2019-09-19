9c65892b8eaafb74c0426489a6142ab9c611a3f7
Daniel Antunes <dantu@lama-e47.univ-savoie.fr>
2019-09-19 12:23:28 +0200

    Fix on CMaje configuration files

--- ext-projects/ExtProjects.cmake
+++ ext-projects/ExtProjects.cmake
@@ -1,5 +1,2 @@
-set(EXTPROJECTS_SOURCE_DIR ${CMAKE_SOURCE_DIR}/ext-projects/source)
-set(EXTPROJECTS_BUILD_DIR ${CMAKE_SOURCE_DIR}/ext-projects/build)
-
 include(ExternalProject)
 include(ext-projects/cmake-files/DIPaCUS.cmake)
